a7334c6e66d44c61ebb6d1551600375d77018628
minnmawoo <minmawoo.ucsm@gmail.com>
2022-08-19 20:14:03 +0630

    deploy

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
         {/* <AnimatePresence exitBeforeEnter> */}
         <Switch location={location} key={location.pathname}>
           <Route exact path="/" component={Main} />
+          <Route exact path="/min-maw" component={Main} />
           <Route exact path="/about" component={AboutPage} />
           <Route exact path="/blog" component={BlogPage} />
           <Route exact path="/work" component={WorkPage} />
