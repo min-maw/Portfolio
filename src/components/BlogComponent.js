6180f2b24f2572baa9d407ca4a563553fe024347
minnmawoo <minmawoo.ucsm@gmail.com>
2022-08-20 00:00:34 +0630

    deploy

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -76,8 +76,8 @@ const BlogComponent = (props) => {
   const { name, tags, date, imgSrc, link } = props.blog;
   return (
     <Container variants={Item}>
-      <Box target="_blank">
-        <a href={link}>
+      <a href={link}>
+        <Box target="_blank">
           <Image img={imgSrc} />
           <Title>{name}</Title>
           <HashTags>
@@ -86,8 +86,8 @@ const BlogComponent = (props) => {
             })}
           </HashTags>
           <Date>{date}</Date>
-        </a>
-      </Box>
+        </Box>
+      </a>
     </Container>
   );
 };
