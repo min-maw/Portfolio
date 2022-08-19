1f82fe822a523c5e35442558bad78e360ab28c84
minnmawoo <minmawoo.ucsm@gmail.com>
2022-08-19 21:39:47 +0630

    deploy

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import './index.css';
-import App from './App';
+import App from "./App";
 // import reportWebVitals from './reportWebVitals';
 
-import '../node_modules/normalize.css'
-import { BrowserRouter } from 'react-router-dom';
+import "../node_modules/normalize.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
